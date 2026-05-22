d526ac76c3ce396b5a3891b01f4ee00589fe3ab2
samehsalah039-beep <samehsalah039@gmail.com>
2026-05-22 21:17:15 +0400

    Add Lessons component placeholder for grammar-hub.

---
+++ src/components/Lessons.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Lessons() {
+  return (
+    <section>
+      <h2>Grammar Lessons</h2>
+      <ul>
+        <li>Nouns and Pronouns</li>
+        <li>Verbs and Tenses</li>
+        <li>Adjectives and Adverbs</li>
+        <li>Sentence Structure</li>
+        <li>Punctuation</li>
+      </ul>
+      <p>More content coming soon!</p>
+    </section>
+  );
+}
