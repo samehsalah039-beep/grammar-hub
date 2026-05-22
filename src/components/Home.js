3a0f68b116b9afec6c60341ff1d430805130d337
samehsalah039-beep <samehsalah039@gmail.com>
2026-05-22 21:16:55 +0400

    Add Home component for grammar-hub.

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <section>
+      <h1>Welcome to the Interactive Grammar Learning Hub</h1>
+      <p>
+        Explore grammar lessons, practice quizzes, and interactive activities tailored for Grade 11 students!
+      </p>
+    </section>
+  );
+}
